0e48dfd6fc19cfaccc581eba16660a7340f7b54c
Kondo Takeo <takeoverjp@gmail.com>
2020-05-24 18:54:08 +0900

    Draw graph.

--- blktrace_viewer/core.py
+++ blktrace_viewer/core.py
@@ -1,8 +1,24 @@
 import sys
 import click
+import logging
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 @click.command()
-@click.option("--verbose", "-v", is_flag=True, default=False)
-def cli(verbose):
-    if verbose:
-        print("Hello World.")
+@click.option("--debug", "-d", is_flag=True, default=False)
+@click.option("--offset", is_flag=True, default=True)
+@click.argument("infile")
+@click.argument("outfile")
+def cli(debug, offset, infile, outfile):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    df = pd.read_csv(infile, sep=" ", names=["time", "latency"])
+
+    if offset:
+        start = df["time"][0]
+        df["time"] -= start
+
+    plt.scatter(df["time"] - df["time"][0], df["latency"], c="red", alpha=0.2)
+    plt.savefig(outfile)
